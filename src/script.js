d66c6f60f8c80ed49369f313aaa6991fa6e44f76
dsound-zz <demiansims@gmail.com>
2019-05-06 00:01:05 -0400

    new changes

--- src/script.js
+++ src/script.js
@@ -15,57 +15,58 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // fetch all cards
 
-    
-    fetch('http://localhost:9000/api/v1/flashcards')
+    const fetchCards = () => {
+        fetch('http://localhost:9000/api/v1/flashcards')
         .then(res => res.json())
         .then(flashcards => {
                 allFlashcards = flashcards
-                renderCards()
-     });
-    
-
-
-    // render cards to DOM
-
-    const renderCards = () => {
-        console.log("in rendercard")
-        return allFlashcards.map(c => {
-            return card.innerHTML += `
-            <div class="container">
-                <div class="card" data-card=${c.id}>
+                return flashcards.map(c => {
+                    return card.innerHTML += `
+                    <div class="container">
+                    <div class="card" data-card=${c.id}>
                     <i class="black delete icon" id="delete-card"></i>
                     <i class="black edit icon" id="edit-card"></i>
                     <h2 class="front" data-id=${c.id}>${c.body_front}</h>
                     <h4 class="back">${c.body_back}</h4>
-                </div>
-            </div>
-           `
-        })
-    };
+                    </div>
+                    </div>
+                    `
+                })
+        });
+    }
+             
+                
+                
+                
+           
+                
 
     // listen for card click and flip
 
-    card.addEventListener('click', e => {
-        if (e.target.dataset.id) {
-            return e.target.parentNode.classList.toggle('flipped')
-        } else if (Array.from(e.target.classList).includes('back')) {
-            return e.target.parentNode.classList.toggle('flipped')
-        }
-    });
+    card.addEventListener('click', e => { 
+        if (e.target.dataset.id) { 
+        return e.target.parentNode.classList.toggle('flipped') 
+        } else if (Array.from(e.target.classList).includes('back')) 
+        { return e.target.parentNode.classList.toggle('flipped') } });
 
     // ****** MODAL SETUP ********
 
     // Modal Events
 
-    modalBtn.addEventListener('click', openModal);
+    modalBtn.addEventListener('click', () => openModal(x=0));
     closeBtn.addEventListener('click', closeModal);
     window.addEventListener('click', outsideClick);
 
     // Open
-    function openModal() {
+    function openModal(x, cardFront, cardBack, foundCard) {
+        console.log(x)
         modal.style.display = 'block';
-        createCard()
-    }
+        if (x === 0) {
+            createCard() 
+        } else {
+            editCard(cardFront, cardBack, foundCard)
+        }
+    };
 
     // Close
     function closeModal() {
@@ -82,10 +83,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // create new card
     const createCard = () => { 
         console.log("in create")
+        let submitBtn = document.getElementById('submit')
+        submitBtn.innerText = "Create"
          form.addEventListener('submit', (e) => {
             e.preventDefault();
-            debugger 
-            console.log("I was cliked too")
             const cardFront = document.getElementById('card-front').value
             const cardBack = document.getElementById('card-back').value
             fetch('http://localhost:9000/api/v1/flashcards', {
@@ -102,6 +103,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 .then(res => res.json())
                 .then(newCard => {
                     allFlashcards.push(newCard)
+                    console.log('after create card: ', allFlashcards)
+                    fetchCards()
                     closeModal()
                 })
          });
@@ -127,26 +130,30 @@ document.addEventListener('DOMContentLoaded', () => {
                 originalCardIndex = allFlashcards.indexOf(foundCard)
                 allFlashcards.splice(originalCardIndex, 1)
                 console.log("in Delete:", allFlashcards)
+                
         };
     });
 
     
     // edit cards 
     card.addEventListener('click', e => {
-        if (e.target.id == "edit-card") {
-            openModal()
-            editCard()
-        }
-     });
-        
-    const editCard = () => {
-        console.log("in editcard")
-        const cardFront = card.querySelector('.front').innerText
-        const cardBack = card.querySelector('.back').innerText 
         const clickedCard = e.target.parentElement
         const foundCard = allFlashcards.find(fcard => {
             return fcard.id == clickedCard.dataset.card
         })
+        
+        if (e.target.id == "edit-card" && foundCard) {
+            console.log("I WAS CLICKED BUT DIDN'T MAKE IT")
+            const cardFront = card.querySelector('.front').innerText
+            const cardBack = card.querySelector('.back').innerText 
+            openModal(x=1, cardFront, cardBack, foundCard)
+            // editCard(cardFront, cardBack, foundCard)
+        }
+     });
+        
+    const editCard = (cardFront, cardBack, foundCard) => {
+        console.log("in editcard")
+       
             document.getElementById('card-front').value = cardFront
             document.getElementById('card-back').value = cardBack
             let submitBtn = document.getElementById('submit')
@@ -175,7 +182,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
     
-    
+   fetchCards() 
 
 
 }); // end DOMContentLoaded
